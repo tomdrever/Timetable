7165bab49df4135600e8fe7a7636bccfa036d8cb
charlesstewart <tomdr1001@outlook.com>
2016-10-09 22:44:45 +0100

    More work on EditPeriod. Getting there.

--- app/src/main/java/tomdrever/timetable/android/fragments/EditPeriodDialogFragment.java
+++ app/src/main/java/tomdrever/timetable/android/fragments/EditPeriodDialogFragment.java
@@ -5,11 +5,17 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import tomdrever.timetable.R;
 import tomdrever.timetable.data.Period;
 
 public class EditPeriodDialogFragment extends DialogFragment {
 	private Period period;
 	private int periodPosition;
+
+	private View view;
 
 	private FragmentSuccessListener successListener;
 
@@ -32,26 +38,46 @@ public class EditPeriodDialogFragment extends DialogFragment {
 
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+		view = getActivity().getLayoutInflater().inflate(R.layout.edit_period_dialog, null);
+
 		// region Set Up Dialog
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setView(view);
 		builder.setTitle(period == null ? "New Period" : String.format("Edit %s", period.getName()));
 		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialogInterface, int i) {
-				// TODO - assign name to period.
-				// TODO - pull other data from UI to period also
-				successListener.onFragmentSuccess(period, periodPosition);
+				EditText editPeriodName = (EditText) view.findViewById(R.id.edit_period_name_edit_text);
+				// Check the timetable has been given a name
+				String name = editPeriodName.getText().toString().trim();
+				if (name.isEmpty()) {
+					// If not, tell the user
+					Toast.makeText(getContext(), "The period needs a name!", Toast.LENGTH_SHORT).show();
+
+					// TODO - highlight name field to the user in some way - USER, DO THIS!
+				} else {
+					successListener.onFragmentSuccess(period, periodPosition);
+				}
 			}
 		});
 		builder.setNegativeButton("Cancel", null);
 		// endregion
 
-		if (period == null) period = new Period("New Period");
-
         return builder.create();
     }
 
-    interface FragmentSuccessListener {
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		// region Set Up UI and Period
+		if (period == null) period = new Period("New Period");
+
+		((EditText) view.findViewById(R.id.edit_period_name_edit_text)).setText(period.getName());
+		// endregion
+	}
+
+	interface FragmentSuccessListener {
 	    void onFragmentSuccess(Period period, int periodPosition);
     }
 }
